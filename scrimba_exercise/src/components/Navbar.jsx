b434a19e4611e1f2a7087019a22e364fcfd5f7f1
WaveThing <justwavethings@pm.me>
2023-04-20 10:21:29 -0400

    Initial commit

---
+++ scrimba_exercise/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+	return (
+		<nav className="Navbar">
+			<img
+				src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+				className="App-logo"
+				alt="logo"
+				height="28.93px"
+			/>
+			<h2>ReactFacts</h2>
+			<h3>React Course - Project 1</h3>
+		</nav>
+	);
+}
+
+export default Navbar;
